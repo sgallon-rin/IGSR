92a396007e097246f45d4820885176ec007eea97
Vincy_King <44465544+Vincy2King@users.noreply.github.com>
2025-04-29 18:13:56 +0800

    Delete clip directory

--- clip/__init__.py
+++
@@ -1,7 +0,0 @@
-from .bert_tokenizer import FullTokenizer
-
-_tokenizer = FullTokenizer()
-# from .new_model import convert_state_dict
-# from .new_utils import load_from_name, available_models, tokenize, image_transform, load
-from .model import convert_state_dict
-from .utils import load_from_name, available_models, tokenize, image_transform, load
